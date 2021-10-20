62047915688722d3c708f4f63f39c84faf481e73
Joseph <thejykim1@gmail.com>
2021-10-20 13:16:06 -0400

    Fixed redirec page

--- pages/[action].js
+++ pages/[action].js
@@ -1,20 +1,32 @@
 import { useRouter } from "next/router";
 import Thread from "../components/thread";
+import { Container, Spinner } from "reactstrap";
 
 const Action = ({ clientID }) => {
 	const router = useRouter();
 	const { action, tid } = router.query;
 
-	if ((action == null) | (tid == null)) {
-		return <p>Whoops! Looks like your link is broken.</p>;
-	} else {
-		return <Thread action={action} tid={tid} clientID={clientID} />;
-	}
+	return (
+		<div>
+			<Container>
+				{router.isReady ? (
+					action && tid ? (
+						<Thread action={action} tid={tid} clientID={clientID} />
+					) : (
+						<p>Whoops! Looks like your link is broken.</p>
+					)
+				) : (
+					<div className="d-flex justify-content-center">
+						<Spinner color="light" />
+					</div>
+				)}
+			</Container>
+		</div>
+	);
 };
 
 export async function getStaticProps(context) {
 	const clientID = process.env.CLIENT_ID;
-	console.log(clientID);
 
 	return {
 		props: {
